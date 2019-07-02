0417136273ba95d81619201e710a26f561531c63
Bayka1997 <nguyenvanbay.ptit@gmail.com>
2019-07-02 18:22:02 +0700

    bay

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
     def hello
-        render html: "Xin chào nguyễn văn bảy"
+        render html: "Xin chào nguyễn văn bảy bay"
     end
 
 end
